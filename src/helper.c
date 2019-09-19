92e80653058e4b6cfb2ec698f64afc85987da651
Fux Projesi <bilgi@fuxproject.org>
2019-09-19 13:00:06 +0000

    Update helper.c

--- src/helper.c
+++ src/helper.c
@@ -5,19 +5,20 @@
 void utf8_decode(char *dst, const char *src) 
 {
     char a, b;
+    if (dst == NULL) return;
     while(*src) 
     {
         if ((*src == '%') && ((a = src[1]) && (b = src[2])) && (isxdigit(a) && isxdigit(b))) 
         {
-            if(a >= 'a') { a -= 'a'-'A'; }
-            if(a >= 'A') { a -= ('A'-10); } else { a -= '0'; }
-            if(b >= 'a') { b -= 'a'-'A'; }
-            if(b >= 'A') { b -= ('A'-10); } else { b -= '0'; }
-            *dst++ = 16*a+b;
+            if (a >= 'a') { a -= 'a'-'A';  }
+            if (a >= 'A') { a -= ('A'-10); } else { a -= '0'; }
+            if (b >= 'a') { b -= 'a'-'A';  }
+            if (b >= 'A') { b -= ('A'-10); } else { b -= '0'; }
+            *dst++ = 16 * a + b;
             src+=3;
-        } 
-        
-        *dst++ = *src++;
+        } else {
+            *dst++ = *src++;
+        }
     }
     *dst++ = '\0';
 }
